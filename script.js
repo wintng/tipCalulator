aa9fda84a2866ed5948216109f336af8e2a5328d
huynh <july31nth@gmail.com>
2026-09-11 22:01:45 -0700

    rework on calculation math

--- script.js
+++ script.js
@@ -6,19 +6,14 @@ const form = document.getElementById("form")
 const calculate = function(){
     const userInputAmount = amountInput.value;
     const userInputTip = tipInput.value;
-    console.log(userInputAmount,userInputTip)
-
-    const tipPercentage = userInputTip/100
-    console.log(`tipPercentage ${tipPercentage}`)
-    const amountToTip = userInputTip*tipPercentage;
-    console.log(`amountToTip ${amountToTip}`)
-
-    const total = userInputAmount + amountToTip
-
+    const total = userInputAmount * (1+userInputTip/100)
+    console.log(total.toFixed(2))
+    return total;
 }
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
+
 })
 
 submit.addEventListener('click', calculate)
